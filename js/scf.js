daefa80f0e165e42a56704bc0c0f29fa02033504
Papik81 <Papik81@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-29 17:52:28 +0000

    Version 1.0.4 upload
    git-svn-id: https://plugins.svn.wordpress.org/dashboard-contact-form/tags/1.0.4@1149308 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/scf.js
@@ -0,0 +1,39 @@
+jQuery( document ).ready(function($) {
+		$("#cdw_content").val(SCF.message);
+		$( "#cdw-support" ).submit(function(event) {
+			event.preventDefault();
+			$( ".scf-ajax").show();
+			 $(".formmessage p").html(SCF.sending);
+
+			var name = $("#cdw_author").val();
+			var title = $("#cdw_title").val();
+			var message = $("#cdw_content").val();
+			var from = $("#cdw_email").val();
+			var nonce = $("#scf_nonce").val();
+			if(message == "" || message == SCF.message || title == "") { 
+				$(".formmessage p").html(SCF.invalid);
+				if (message == "" || message == SCF.message) $("#cdw_content").css({"border-color": "red"});
+				if (title == "") $("#cdw_title").css({"border-color": "red"});
+				$( ".scf-ajax").hide();			
+			}	
+			else {
+			var data =  { action :	'cdw_send_message', 'cdw_nonce': nonce, 'author_name': name, 'author_email': from, 'mail_title': title, 'mail_content': message};
+			$("#cdw_content").removeAttr('style');
+			$("#cdw_title").removeAttr('style');
+			var posting = $.post( ajaxurl, data,function(response){
+				$(".formmessage p").html(response);
+			})
+			.always(function (){
+				$( ".scf-ajax").hide(); 
+			}).done(function(){
+				$("#cdw_title").val("");
+				$("#cdw_content").val("");
+				setTimeout( function(){ 
+					$(".formmessage p").fadeOut(500,function(){
+					$(this).html("");
+					$(this).show();});},3000 );
+			});}
+			
+			return false;
+	});	
+});
